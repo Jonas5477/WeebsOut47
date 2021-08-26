1ee009b64eef335f0220470fe34456065fe487be
Jonas Fuhrwerk <jonas.fuhrwerk@gmail.com>
2021-08-26 18:48:28 +0200

    Projektdateien hinzufügen.

---
+++ WeebsOut47/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeebsOut47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
