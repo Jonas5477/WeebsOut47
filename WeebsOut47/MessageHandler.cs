99d2520b6149093154dcd84bed54f98bb64a1033
Jonas Fuhrwerk <jonas.fuhrwerk@gmail.com>
2021-09-03 01:15:20 +0200

    time stamps gefixed

--- WeebsOut47/MessageHandler.cs
+++ WeebsOut47/MessageHandler.cs
@@ -15,8 +15,10 @@ namespace WeebsOut47.Twitch.Messages
             WeebsOut = bot;
         }
 
-        private static long _timeStamp = 0;
-        private static string _user;
+        private static long _ayaya = 0;
+        private static long _user = 0;
+        private static long _mods = 0;
+        private static long _bat = 0;
         Dictionary<long, string> openWith = new();
 
         public void Handle()
@@ -47,7 +49,7 @@ namespace WeebsOut47.Twitch.Messages
                 }
                 if (message.StartsWith("test"))
                 {
-                    WeebsOut.Client.SendMessage(ChatMessage.Channel, $"{ChatMessage.IsSubscriber}");
+                    WeebsOut.Client.SendMessage(ChatMessage.Channel, $"{ChatMessage.TmiSentTs}");
                 }
                 if (message.StartsWith("sub"))
                 {
@@ -62,15 +64,16 @@ namespace WeebsOut47.Twitch.Messages
                 }
                 if (ChatMessage.IsModerator)
                 {
-                    if (600000 < DateTimeOffset.Now.ToUnixTimeMilliseconds() - _timeStamp)
+                    if (600000 < DateTimeOffset.Now.ToUnixTimeMilliseconds() - _mods)
                     {
                         WeebsOut.Client.SendMessage(ChatMessage.Channel, " M OMEGALUL DS");
+                        _mods = DateTimeOffset.Now.ToUnixTimeMilliseconds();
                     }
                 }
             }
             if (ChatMessage.Message.ToLower().Contains("ayaya"))
             {
-                if (600000 < DateTimeOffset.Now.ToUnixTimeMilliseconds() - _timeStamp)
+                if (600000 < DateTimeOffset.Now.ToUnixTimeMilliseconds() - _ayaya)
                 {
                     if (ChatMessage.IsModerator)
                     {
@@ -80,24 +83,25 @@ namespace WeebsOut47.Twitch.Messages
                     {
                         WeebsOut.Client.SendMessage(ChatMessage.Channel, $"/timeout {ChatMessage.Username} 10s");
                     }
+                    _ayaya = DateTimeOffset.Now.ToUnixTimeMilliseconds();
                 }
             }
             if (Accountinfo.Weebs.Split().ToList().Contains(ChatMessage.Username))
             {
-                if (60000000 < DateTimeOffset.Now.ToUnixTimeMilliseconds() - _timeStamp)
+                if (60000000 < DateTimeOffset.Now.ToUnixTimeMilliseconds() - _user)
                 {
                     WeebsOut.Client.SendMessage(ChatMessage.Channel, $"We lost {ChatMessage.Username} PepeHands");
-                    _timeStamp = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                    _user = DateTimeOffset.Now.ToUnixTimeMilliseconds();
                 }
             }
             if (ChatMessage.Message.ToLower().Contains(" batchest "))
             {
-                if (60000000 < DateTimeOffset.Now.ToUnixTimeMilliseconds() - _timeStamp)
+                if (60000000 < DateTimeOffset.Now.ToUnixTimeMilliseconds() - _bat)
                 {
                     if (ChatMessage.Username == " jann_amh_ ")
                     {
                         WeebsOut.Client.SendMessage(ChatMessage.Channel, $" JannGa ");
-                        _timeStamp = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                        _bat = DateTimeOffset.Now.ToUnixTimeMilliseconds();
                     }
                 }
             }
