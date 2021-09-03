68f1a4d62268db404daebb03fd7f7378c3eecf90
Jonas Fuhrwerk <jonas.fuhrwerk@gmail.com>
2021-09-03 19:51:43 +0200

    dreck

--- WeebsOut47/MessageHandler.cs
+++ WeebsOut47/MessageHandler.cs
@@ -42,8 +42,7 @@ namespace WeebsOut47.Twitch.Messages
                 }
                 if (message.StartsWith("uptime"))
                 {
-                    Bot.Uptime(DateTimeOffset.Now.ToUnixTimeMilliseconds(), Bot._timer);
-                    WeebsOut.Client.SendMessage(ChatMessage.Channel, $"WeebsOut47 ist jetzt seit ");
+                     Bot.Uptime(DateTimeOffset.Now.ToUnixTimeMilliseconds(), Bot._timer);
                 }
                 if (message.StartsWith("color"))
                 {
