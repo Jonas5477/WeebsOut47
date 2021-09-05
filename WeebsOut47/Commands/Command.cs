692d67b3c00ed39f981602016a111d3dbd2318c8
Jonas Fuhrwerk <jonas.fuhrwerk@gmail.com>
2021-09-06 00:21:45 +0200

    command command

---
+++ WeebsOut47/Commands/Command.cs
@@ -0,0 +1,16 @@
+﻿using TwitchLib.Client.Models;
+
+namespace WeebsOut47.Commands
+{
+    public abstract class Command 
+    {
+        public Bot WeebsOut { get; }
+        public ChatMessage ChatMessage { get; }
+        public Command(Bot weebsout, ChatMessage chatMessage)
+        {
+            WeebsOut = weebsout;
+            ChatMessage = chatMessage;
+        }
+        public abstract void SendMessage();
+    }
+}
