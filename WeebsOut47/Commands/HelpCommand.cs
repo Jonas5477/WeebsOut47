d2e2e996daefad226d7bd5d7e9e8c0fec8e4ff16
Jonas Fuhrwerk <jonas.fuhrwerk@gmail.com>
2021-09-06 00:22:32 +0200

    helpcommand

---
+++ WeebsOut47/Commands/HelpCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib.Client.Models;
+
+namespace WeebsOut47.Commands
+{
+    class HelpCommand : Command
+    {
+        public HelpCommand(Bot weebsout, ChatMessage chatMessage) : base(weebsout, chatMessage)
+        {
+        }
+        public override void SendMessage()
+        {
+            WeebsOut.Client.SendMessage(ChatMessage.Channel, $"All posible commands 👉 github.com/Jonas5477/WeebsOut47/blob/master/README.md");
+        }
+    }
+}
