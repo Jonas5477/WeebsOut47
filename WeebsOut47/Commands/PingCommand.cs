45735d7430ea2f1d5cfe7282213df99328c27b33
Jonas Fuhrwerk <jonas.fuhrwerk@gmail.com>
2021-09-06 00:22:47 +0200

    pingcommand

---
+++ WeebsOut47/Commands/PingCommand.cs
@@ -0,0 +1,16 @@
+﻿using TwitchLib.Client.Models;
+
+namespace WeebsOut47.Commands
+{
+    class PingCommand : Command
+    {
+        public PingCommand(Bot weebsout, ChatMessage chatMessage) : base(weebsout, chatMessage)
+        {
+        }
+
+        public override void SendMessage()
+        {
+            WeebsOut.Client.SendMessage(ChatMessage.Channel, "DansGame ");
+        }
+    }
+}
