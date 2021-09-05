3b65cd2bcc01b168663c6dd3c8150baa2ba4cb8f
Jonas Fuhrwerk <jonas.fuhrwerk@gmail.com>
2021-09-05 22:51:36 +0200

    chatter api

---
+++ WeebsOut47/ApiRequests.cs
@@ -0,0 +1,30 @@
+﻿using HLE.Collections;
+using HLE.Strings;
+using System.Collections.Generic;
+using System.Text.Json;
+namespace WeebsOut47
+{
+    class ApiRequests
+    {
+        public static int GetChatterCount(string channel)
+        {
+            HttpGet request = new($"https://tmi.twitch.tv/group/user/{channel.Remove("#")}/chatters");
+            return request.Data.GetProperty("chatter_count").GetInt32();
+        }
+
+        public static List<string> GetChatters(string channel)
+        {
+            HttpGet request = new($"https://tmi.twitch.tv/group/user/{channel.Remove("#")}/chatters");
+            JsonElement chatters = request.Data.GetProperty("chatters");
+            List<string> result = new();
+            new string[] { "broadcaster", "vips", "moderators", "staff", "admins", "global_mods", "viewers" }.ForEach(p =>
+            {
+                for (int i = 0; i < chatters.GetProperty(p).GetArrayLength(); i++)
+                {
+                    result.Add(chatters.GetProperty(p)[i].ToString());
+                }
+            });
+            return result;
+        }
+    }
+}
