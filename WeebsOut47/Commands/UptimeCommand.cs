1cb2b1818feafecbd47fdeed64e12b0ba3627430
Jonas Fuhrwerk <jonas.fuhrwerk@gmail.com>
2021-09-06 00:23:03 +0200

    uptimecommand

---
+++ WeebsOut47/Commands/UptimeCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib.Client.Models;
+
+namespace WeebsOut47.Commands
+{
+    class UptimeCommand : Command
+    {
+        public UptimeCommand(Bot weebsout, ChatMessage chatMessage) : base(weebsout, chatMessage)
+        {
+        }
+
+        public override void SendMessage()
+        {
+            Bot.Uptime(DateTimeOffset.Now.ToUnixTimeMilliseconds(), Bot._timer);
+        }
+    }
+}
