cc3fe500b9e162920f42ca54bf13a5fd4d129b19
Jonas Fuhrwerk <jonas.fuhrwerk@gmail.com>
2021-09-06 00:21:31 +0200

    colorcommand

---
+++ WeebsOut47/Commands/ColorCommand.cs
@@ -0,0 +1,18 @@
+﻿
+
+using TwitchLib.Client.Models;
+
+namespace WeebsOut47.Commands
+{
+    class ColorCommand : Command
+    {
+        public ColorCommand(Bot weebsout, ChatMessage chatMessage) : base(weebsout, chatMessage)
+        {
+        }
+
+        public override void SendMessage()
+        {
+            WeebsOut.Client.SendMessage(ChatMessage.Channel, $"Your {ChatMessage.Color} has the Hex Code {ChatMessage.ColorHex}");
+        }
+    }
+}
