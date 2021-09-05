ecc1f5f8e33008ee06a8f1b2f1b3d10daa57fdd5
Jonas Fuhrwerk <jonas.fuhrwerk@gmail.com>
2021-09-06 00:22:24 +0200

    idcommand

---
+++ WeebsOut47/Commands/IdCommand.cs
@@ -0,0 +1,17 @@
+﻿
+using TwitchLib.Client.Models;
+
+namespace WeebsOut47.Commands
+{
+    class IdCommand : Command
+    {
+        public IdCommand(Bot weebsout, ChatMessage chatMessage) : base(weebsout, chatMessage)
+        {
+        }
+
+        public override void SendMessage()
+        {
+            WeebsOut.Client.SendMessage(ChatMessage.Channel, $"Your ID is {ChatMessage.UserId} and the ID of this Chat Room is {ChatMessage.RoomId} FeelsOkayMan");
+        }
+    }
+}
