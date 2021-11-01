ec0fb7ebad0d35be7ff1f009134311f471eb757c
Jonas <jonas.fuhrwerk@gmail.com>
2021-11-02 00:25:03 +0100

    new API- requests :)

--- WeebsOut47/ApiRequests.cs
+++ WeebsOut47/ApiRequests.cs
@@ -3,10 +3,62 @@ using HLE.Strings;
 using System.Collections.Generic;
 using System.Text.Json;
 using HLE.HttpRequests;
+using System;
+using System.Web;
+using MySqlX.XDevAPI.Common;
+
 namespace WeebsOut47
 {
     class ApiRequests
     {
+        private const string _randomFox = "https://randomfox.ca/floof/";
+        private const string _bored = "https://www.boredapi.com/api/activity/";
+        private const string _joke = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist";
+        public static string GetJoke()
+        {
+            HttpGet request = new HttpGet(_joke);
+            string type = request.Data.GetProperty("type").GetString();
+            if (request.ValidJsonData)
+            {
+                if(type == "twopart")
+                {
+                    return $"{request.Data.GetProperty("setup").GetString()} PauseChamp {request.Data.GetProperty("delivery").GetString()} haHAA";                   
+                }
+                else
+                {
+                    return $"{request.Data.GetProperty("joke").GetString()} haHAA";
+                }
+            }
+            else
+            {
+                return request.Result;
+            }
+        }
+        public static  string GetBored()
+        {
+            HttpGet request = new (_bored);
+            string activity = request.Data.GetProperty("activity").GetString();
+            if (request.ValidJsonData)
+            {
+                return $"{activity} monkaUOMEGA";
+            }
+            else
+            {
+                return request.Result;
+            }
+        }
+        public static string GetRandomFox()
+        {
+            HttpGet request = new (_randomFox);
+            if (request.ValidJsonData)
+            {
+                return request.Data.GetProperty("image").GetString();
+            }
+            else 
+            {
+                return request.Result;
+            }
+        }
         public static int GetChatterCount(string channel)
         {
             HttpGet request = new($"https://tmi.twitch.tv/group/user/{channel.Remove("#")}/chatters");
@@ -17,7 +69,7 @@ namespace WeebsOut47
             HttpGet request = new($"https://tmi.twitch.tv/group/user/{channel.Remove("#")}/chatters");
             JsonElement chatters = request.Data.GetProperty("chatters");
             List<string> result = new();
-            new string[] { "broadcaster", "vips", "moderators", "staff", "admins", "global_mods", "viewers" }.ForEach(p =>
+            new string[]{ "broadcaster", "vips", "moderators", "staff", "admins", "global_mods", "viewers" }.ForEach(p =>
             {
                 for (int i = 0; i < chatters.GetProperty(p).GetArrayLength(); i++)
                 {
@@ -25,6 +77,29 @@ namespace WeebsOut47
                 }
             });
             return result;
+        }
+        public static string GetMath(string rechnung)
+        {
+            rechnung = HttpUtility.UrlEncode(rechnung);
+            HttpGet request = new($"http://api.mathjs.org/v4/?expr={rechnung}");
+            return request.Result;
+        }
+        public static string GetWeather(string city)
+        {
+            HttpGet request = new($"https://goweather.herokuapp.com/weather/{city}");
+            string temperature = request.Data.GetProperty("temperature").GetString();
+            string wind = request.Data.GetProperty("wind").GetString();
+            string description = request.Data.GetProperty("description").GetString();
+            if (request.ValidJsonData)
+            {
+                return $"In {city.ToUpper()[0] + city.Substring(1)} the temperature is {temperature}. The wind velocity is {wind} and the weather is {description}";
+            }
+            else
+            {
+                return request.Result;
+            }
+            
+
         }
     }
 }
