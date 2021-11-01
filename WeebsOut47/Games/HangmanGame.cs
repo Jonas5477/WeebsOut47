67fafc2328cf86adc96e67dd4a4fb64216b6bdfa
Jonas <jonas.fuhrwerk@gmail.com>
2021-11-02 00:26:20 +0100

    hangman game started

---
+++ WeebsOut47/Games/HangmanGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace WeebsOut47.Games
+{
+    public class HangmanGame
+    {
+        public static List<HangmanGame> HangManGames { get; } = new();
+        public string Channel { get; }
+        public string User { get; }
+        public string Keyword { get; set; }
+        public int Lives { get; private set; }
+        private const byte _maxhealth = 10;
+        public HangmanGame(string channel, string user)
+        {
+            Channel = channel;
+            User = user;
+        }      
+        //public string GetHearts()
+        //{
+        //    String[] Hearts = new string[_maxhealth];
+        //}
+    }
+}
