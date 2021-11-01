fec655756a346ba2d968f37e6513aff3d17b06ab
Jonas <jonas.fuhrwerk@gmail.com>
2021-11-02 00:28:00 +0100

    fixed Batchest command

--- WeebsOut47/MessageCommands/BatChestCommand.cs
+++ WeebsOut47/MessageCommands/BatChestCommand.cs
@@ -13,13 +13,14 @@ namespace WeebsOut47.MessageCommands
 
         public override void SendMessage()
         {
-            if (60000000 < DateTimeOffset.Now.ToUnixTimeMilliseconds() - _bat)
+            if (600000 < DateTimeOffset.Now.ToUnixTimeMilliseconds() - _bat)
             {
                 if (ChatMessage.Username == "jann_amh_")
                 {
-                    WeebsOut.Client.SendMessage(ChatMessage.Channel, $"JannGa");
-                    _bat = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-                }
+                        Console.WriteLine("jann hat geschrieben");                 
+                        WeebsOut.Client.SendMessage(ChatMessage.Channel, $"JannGa");
+                        _bat = DateTimeOffset.Now.ToUnixTimeMilliseconds();                  
+                }      
             }
         }
     }
