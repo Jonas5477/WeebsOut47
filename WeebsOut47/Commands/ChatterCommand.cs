a2e39def55309fdbe4d99d94da2b6316a0f57af0
Jonas <jonas.fuhrwerk@gmail.com>
2021-11-02 00:30:01 +0100

    new command: Outputs all chatters from a chat room

---
+++ WeebsOut47/Commands/ChatterCommand.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HLE.Emojis;
+using HLE.Strings;
+using TwitchLib.Client.Models;
+using WeebsOut47.Utilities;
+
+namespace WeebsOut47.Commands
+{
+    class ChatterCommand : Command
+    {
+        public ChatterCommand(Bot weebsout, ChatMessage chatMessage) : base(weebsout, chatMessage)
+        {
+        }
+        public override void SendMessage()
+        {
+            string message = ChatMessage.Message[1..].ToLower();
+            if (message.StartsWith("chatter"))
+            {
+                string channel;
+                if (ChatMessage.Message.Split().Length > 1)
+                {
+                    channel = message.Split()[1];
+                }
+                else
+                {
+                    channel = ChatMessage.Channel;
+                }
+                List<string> chatter = ApiRequests.GetChatters(channel).Select(c=> c.Antiping()).ToList();
+                WeebsOut.Client.SendMessage(ChatMessage.Channel, $"In dem chat von {channel} sind {chatter.Count} Chatter {Emoji.PointRight} {chatter.ToSequence()}");
+            }
+        }
+    }
+}
