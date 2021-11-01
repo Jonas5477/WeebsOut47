6094d1c374a572fa2437482c17d9e6abfa7cd6b2
Jonas <jonas.fuhrwerk@gmail.com>
2021-11-02 00:31:29 +0100

    new command to calculate

---
+++ WeebsOut47/Commands/MathCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TwitchLib.Client.Models;
+
+namespace WeebsOut47.Commands
+{
+    class MathCommand : Command
+    {
+        public MathCommand(Bot weebsout, ChatMessage chatMessage) : base(weebsout, chatMessage)
+        {
+        }
+
+        public override void SendMessage()
+        {
+            string rechnung = ChatMessage.Message[(ChatMessage.Message.Split()[0].Length + 1)..];
+            WeebsOut.Client.SendMessage(ChatMessage.Channel, $"{rechnung} = {ApiRequests.GetMath(rechnung)}");
+        }
+    }
+}
