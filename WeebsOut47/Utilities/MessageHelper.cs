6ae24a722278756a53cbae1780b3a6347437d5a5
Jonas <jonas.fuhrwerk@gmail.com>
2021-11-10 16:47:13 +0100

    message/stringhelper

---
+++ WeebsOut47/Utilities/MessageHelper.cs
@@ -0,0 +1,13 @@
+﻿using HLE.Strings;
+using System.Collections.Generic;
+
+namespace WeebsOut47.Utilities
+{
+    public static class MessageHelper
+    {
+        public static string Prepare(this string input)
+        {
+            return input.Remove("󠀀").TrimAll();
+        }
+    }
+}
