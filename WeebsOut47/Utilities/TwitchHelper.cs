ea06d604cf152b36d0e91dff09d3f29e40084040
Jonas <jonas.fuhrwerk@gmail.com>
2021-11-10 16:46:34 +0100

    twitchhelper

---
+++ WeebsOut47/Utilities/TwitchHelper.cs
@@ -0,0 +1,12 @@
+﻿using TwitchLib.Client.Models;
+
+namespace WeebsOut47.Utilities
+{
+    public static class TwitchHelper
+    {
+        public static string GetMessage(this ChatMessage chatMessage)
+        {
+            return chatMessage.Message.Prepare();
+        }
+    }
+}
