cc39ab64161f9421847468d3104d43701e6f086b
Jonas <jonas.fuhrwerk@gmail.com>
2021-11-11 23:26:25 +0100

    new class

---
+++ WeebsOut47/APIs/WeatherAPI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace WeebsOut47.APIs
+{
+    public class WeatherAPI
+    {
+        [JsonPropertyName("weather")]
+        public List<WeatherProp> Weather { get; set; }
+        [JsonPropertyName("main")]
+        public MainProperty Main { get; set; }
+        [JsonPropertyName("wind")]
+        public WindProperty Wind { get; set; }
+        [JsonPropertyName("clouds")]
+        public CloudProperty Clouds { get; set; }
+        [JsonPropertyName("sys")]
+        public SysProperty Sys { get; set; }
+        [JsonPropertyName("timezone")]
+        public int Timezone { get; set; }
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("coord")]
+        public CoordProperty Coord { get; set; }
+        [JsonPropertyName("base")]
+        public string Base { get; set; }
+        [JsonPropertyName("visibility")]
+        public int Visibility  { get; set; }
+        [JsonPropertyName("dt")]
+        public int Dt  { get; set; }
+        [JsonPropertyName("cod")]
+        public int Cod { get; set; }
+    }
+}
